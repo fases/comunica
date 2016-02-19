8aff81619e68df364477f8d5a066f76bade0603c
Jaadsonm <jaadson_medeiros@hotmail.com>
2016-02-19 03:45:38 -0200

    Atualização SQL e Diagramas
    Adequação do SQL; correção de inconsistências; atualização do diagrama;

--- comunica.sql
+++ comunica.sql
@@ -1,23 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tempo de Geração: 
--- Versão do Servidor: 5.5.27
--- Versão do PHP: 5.4.7
+-- Host: 127.0.0.1
+-- Generation Time: 19-Fev-2016 às 04:17
+-- Versão do servidor: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de Dados: `comunica`
+-- Database: `comunica`
 --
 
 -- --------------------------------------------------------
@@ -26,22 +26,21 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `emprestimo`
 --
 
-CREATE TABLE IF NOT EXISTS `emprestimo` (
+CREATE TABLE `emprestimo` (
   `data` date NOT NULL,
   `hora` time NOT NULL,
   `id_material` int(11) NOT NULL,
   `justificativa` varchar(255) NOT NULL,
   `obs` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `data_devolucao` int(11) NOT NULL,
   `hora_devolucao` time NOT NULL,
   `local` varchar(255) NOT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `termos` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `termos` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,19 +48,18 @@ CREATE TABLE IF NOT EXISTS `emprestimo` (
 -- Estrutura da tabela `eventos`
 --
 
-CREATE TABLE IF NOT EXISTS `eventos` (
+CREATE TABLE `eventos` (
   `data` date NOT NULL,
   `hora` time NOT NULL,
   `responsavel` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `local` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `descricao` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `obs` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=13 ;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Extraindo dados da tabela `eventos`
@@ -76,11 +74,11 @@ INSERT INTO `eventos` (`data`, `hora`, `responsavel`, `local`, `descricao`, `obs
 -- Estrutura da tabela `impressao`
 --
 
-CREATE TABLE IF NOT EXISTS `impressao` (
+CREATE TABLE `impressao` (
   `tipo_material` varchar(255) NOT NULL,
   `tipo_papel` varchar(255) NOT NULL,
   `justificativa` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `url_1` varchar(255) DEFAULT NULL,
   `url_2` varchar(255) DEFAULT NULL,
@@ -89,9 +87,8 @@ CREATE TABLE IF NOT EXISTS `impressao` (
   `arquivo_1` varchar(255) DEFAULT NULL,
   `arquivo_3` varchar(255) DEFAULT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `arquivo_2` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `arquivo_2` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `impressao`
@@ -107,16 +104,15 @@ INSERT INTO `impressao` (`tipo_material`, `tipo_papel`, `justificativa`, `id`, `
 -- Estrutura da tabela `material`
 --
 
-CREATE TABLE IF NOT EXISTS `material` (
+CREATE TABLE `material` (
   `especificacao` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `nome` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `situacao` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `patrimonio` int(11) NOT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=26 ;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Extraindo dados da tabela `material`
@@ -137,8 +133,8 @@ INSERT INTO `material` (`especificacao`, `nome`, `id`, `status`, `situacao`, `pa
 -- Estrutura da tabela `noticias`
 --
 
-CREATE TABLE IF NOT EXISTS `noticias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL,
   `titulo` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `descricao` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `arquivo_1` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
@@ -150,11 +146,8 @@ CREATE TABLE IF NOT EXISTS `noticias` (
   `url_3` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `id_usuario` int(11) NOT NULL,
   `data_publicacao` date NOT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `status` (`status`),
-  KEY `status_2` (`status`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -162,11 +155,11 @@ CREATE TABLE IF NOT EXISTS `noticias` (
 -- Estrutura da tabela `producao`
 --
 
-CREATE TABLE IF NOT EXISTS `producao` (
-  `data_publicacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE `producao` (
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_usuario` int(11) NOT NULL,
   `justificativa` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `url_1` varchar(255) DEFAULT NULL,
   `url_2` varchar(255) DEFAULT NULL,
@@ -174,15 +167,14 @@ CREATE TABLE IF NOT EXISTS `producao` (
   `arquivo_1` varchar(255) DEFAULT NULL,
   `arquivo_2` varchar(255) DEFAULT NULL,
   `arquivo_3` varchar(255) DEFAULT NULL,
-  `tipo` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+  `tipo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `producao`
 --
 
-INSERT INTO `producao` (`data_publicacao`, `id_usuario`, `justificativa`, `id`, `status`, `url_1`, `url_2`, `url_3`, `arquivo_1`, `arquivo_2`, `arquivo_3`, `tipo`) VALUES
+INSERT INTO `producao` (`data_cadastro`, `id_usuario`, `justificativa`, `id`, `status`, `url_1`, `url_2`, `url_3`, `arquivo_1`, `arquivo_2`, `arquivo_3`, `tipo`) VALUES
 ('0000-00-00 00:00:00', 1, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ', 8, 1, NULL, NULL, NULL, NULL, NULL, NULL, 'Banner (120mmx80mm)'),
 ('0000-00-00 00:00:00', 1, 'Testando as 14:32', 9, 1, NULL, NULL, NULL, NULL, NULL, NULL, 'Folder (A4)');
 
@@ -192,8 +184,8 @@ INSERT INTO `producao` (`data_publicacao`, `id_usuario`, `justificativa`, `id`, 
 -- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
   `nome` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `senha` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `matricula` varchar(255) COLLATE latin1_general_ci NOT NULL,
@@ -203,9 +195,8 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `status` int(1) NOT NULL,
   `endereco` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `informacao` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=20 ;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Extraindo dados da tabela `usuario`
@@ -214,6 +205,93 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 INSERT INTO `usuario` (`id`, `nome`, `senha`, `matricula`, `email`, `telefone`, `tipo_acesso`, `status`, `endereco`, `informacao`, `data_cadastro`) VALUES
 (19, 'Ana Carolina', 'e10adc3949ba59abbe56e057f20f883e', '20121044', 'anacarolina@gmail.com', '(84) 99999-9999', 2, 0, NULL, 'Lorem ipsum dolor sit amet, 04', '2016-02-18 17:32:45');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `impressao`
+--
+ALTER TABLE `impressao`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status` (`status`),
+  ADD KEY `status_2` (`status`);
+
+--
+-- Indexes for table `producao`
+--
+ALTER TABLE `producao`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `impressao`
+--
+ALTER TABLE `impressao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `material`
+--
+ALTER TABLE `material`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+--
+-- AUTO_INCREMENT for table `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `producao`
+--
+ALTER TABLE `producao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
