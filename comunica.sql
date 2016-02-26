bef1d0e96e02c4ceb05a60d4183d580bf2e3db79
Jaadsonm <jaadson_medeiros@hotmail.com>
2016-02-26 18:39:55 -0300

    Atualização SQL

--- comunica.sql
+++ comunica.sql
@@ -1,101 +1,77 @@
-CREATE DATABASE  IF NOT EXISTS `comunica` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `comunica`;
--- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: comunica
--- ------------------------------------------------------
--- Server version	5.7.9
+-- Host: 127.0.0.1
+-- Generation Time: 26-Fev-2016 às 22:29
+-- Versão do servidor: 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `emprestimo`
+-- Database: `comunica`
 --
 
-DROP TABLE IF EXISTS `emprestimo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `emprestimo` (
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `emprestimo`
+--
+
+CREATE TABLE IF NOT EXISTS `emprestimo` (
   `data` date NOT NULL,
   `hora` time NOT NULL,
   `id_material` int(11) NOT NULL,
   `justificativa` varchar(255) NOT NULL,
   `obs` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `data_devolucao` date NOT NULL,
   `hora_devolucao` time NOT NULL,
-  `local` varchar(255) NOT NULL,
+  `id_local` int(11) NOT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `termos` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `termos` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `emprestimo`
+-- Estrutura da tabela `eventos`
 --
 
-LOCK TABLES `emprestimo` WRITE;
-/*!40000 ALTER TABLE `emprestimo` DISABLE KEYS */;
-INSERT INTO `emprestimo` VALUES ('2016-02-19','12:00:00',19,'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ','Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',1,1,1,'2016-02-19','16:00:00','B1','2016-02-19 08:45:27',1);
-/*!40000 ALTER TABLE `emprestimo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `eventos`
---
-
-DROP TABLE IF EXISTS `eventos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `eventos` (
+CREATE TABLE IF NOT EXISTS `eventos` (
   `data` date NOT NULL,
   `hora` time NOT NULL,
   `responsavel` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `local` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `descricao` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `obs` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `eventos`
+-- Estrutura da tabela `impressao`
 --
 
-LOCK TABLES `eventos` WRITE;
-/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
-INSERT INTO `eventos` VALUES ('2016-02-18','15:55:00','Carol','Auditório','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ',12,1,1,'2016-02-18 16:50:59');
-/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `impressao`
---
-
-DROP TABLE IF EXISTS `impressao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `impressao` (
+CREATE TABLE IF NOT EXISTS `impressao` (
   `tipo_material` varchar(255) NOT NULL,
   `tipo_papel` varchar(255) NOT NULL,
   `justificativa` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `url_1` varchar(255) DEFAULT NULL,
   `url_2` varchar(255) DEFAULT NULL,
@@ -104,59 +80,45 @@ CREATE TABLE `impressao` (
   `arquivo_1` varchar(255) DEFAULT NULL,
   `arquivo_3` varchar(255) DEFAULT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `arquivo_2` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `arquivo_2` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `impressao`
+-- Estrutura da tabela `local`
 --
 
-LOCK TABLES `impressao` WRITE;
-/*!40000 ALTER TABLE `impressao` DISABLE KEYS */;
-INSERT INTO `impressao` VALUES ('folder','Fotográfico','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ',1,1,NULL,NULL,NULL,1,NULL,NULL,'0000-00-00 00:00:00',NULL),('banner','A4','asdhasjdhsadjkdhsdjdk',2,1,NULL,NULL,NULL,2,NULL,NULL,'2016-02-17 18:39:39',NULL);
-/*!40000 ALTER TABLE `impressao` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `local` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `predio` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `material`
+-- Estrutura da tabela `material`
 --
 
-DROP TABLE IF EXISTS `material`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `material` (
+CREATE TABLE IF NOT EXISTS `material` (
   `especificacao` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `nome` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `situacao` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `patrimonio` int(11) NOT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `material`
+-- Estrutura da tabela `noticias`
 --
 
-LOCK TABLES `material` WRITE;
-/*!40000 ALTER TABLE `material` DISABLE KEYS */;
-INSERT INTO `material` VALUES ('CÂMERA FOTOGRÁFICA DIGITAL, REFLEX, COM AF/AE, COM SENSOR DIGITAL CMOS DE 18 MEGAPIXELS COM PROPORÇÃO DE 3:2, LENTE 18-55MM F/3.5-5.6, VISOR ÓTICO, MÍDIAS DE GRAVAÇÃO COM CARTÃO DO TIPO SD, SDHC E SDXC, FORMATO DE IMAGEM JPEG, RAW E JPEG+RAW, GARANTIA DE ','Câmera digital  CANON',19,0,'1',221117,'2016-02-18 16:32:57'),('FILMADORA DIGITAL PORTÁTIL COR PREDOMINANTE PRETO COM DISCO RÍGIDO INTERNO DE 16GB. TELA LCD DE NO MÍNIMO 2,7\", RESOLUÇÃO DE 3,3 MP, ZOOM ÓPTICO: 25X. MARCA: SONY, MODELO: HDR CX160.','Filmadora SONY',20,0,'1',227960,'2016-02-18 16:35:58'),('Refletor redondo de cor vermelha','Canhão de Luz (cor vermelha)',21,0,'1',0,'2016-02-18 16:41:17'),('Refletor redondo de cor laranja','Canhão de Luz (cor laranja)',22,0,'1',0,'2016-02-18 16:42:58'),('Refletor redondo de cor violeta','Canhão de Luz (cor violeta)',23,0,'1',0,'2016-02-18 16:43:12'),('Refletor redondo ','Refletor redondo ',24,0,'1',0,'2016-02-18 16:44:22'),('Mesa de controle de iluminação(compacta), c/02 presets, de 12 faders(potenciômetros) cada, com possibilidade de controlar até 48 canais, 12 páginas de 24 memórias. Marca: STAR.','Mesa de controle de iluminação',25,0,'1',110260,'2016-02-18 16:44:33');
-/*!40000 ALTER TABLE `material` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `noticias`
---
-
-DROP TABLE IF EXISTS `noticias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `noticias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `noticias` (
+`id` int(11) NOT NULL,
   `titulo` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `descricao` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `arquivo_1` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
@@ -168,35 +130,20 @@ CREATE TABLE `noticias` (
   `url_3` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `id_usuario` int(11) NOT NULL,
   `data_publicacao` date NOT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `status` (`status`),
-  KEY `status_2` (`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `noticias`
+-- Estrutura da tabela `producao`
 --
 
-LOCK TABLES `noticias` WRITE;
-/*!40000 ALTER TABLE `noticias` DISABLE KEYS */;
-INSERT INTO `noticias` VALUES (1,'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dol',NULL,NULL,NULL,1,NULL,NULL,NULL,1,'2016-02-19','2016-02-19 07:40:15');
-/*!40000 ALTER TABLE `noticias` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `producao`
---
-
-DROP TABLE IF EXISTS `producao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `producao` (
+CREATE TABLE IF NOT EXISTS `producao` (
   `data_publicacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_usuario` int(11) NOT NULL,
   `justificativa` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `url_1` varchar(255) DEFAULT NULL,
   `url_2` varchar(255) DEFAULT NULL,
@@ -205,30 +152,17 @@ CREATE TABLE `producao` (
   `arquivo_2` varchar(255) DEFAULT NULL,
   `arquivo_3` varchar(255) DEFAULT NULL,
   `tipo` varchar(255) NOT NULL,
-  `data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `producao`
+-- Estrutura da tabela `usuario`
 --
 
-LOCK TABLES `producao` WRITE;
-/*!40000 ALTER TABLE `producao` DISABLE KEYS */;
-INSERT INTO `producao` VALUES ('2016-02-22 20:08:12',1,'advvadav',10,1,NULL,NULL,NULL,'/Library/WebServer/Documents/comunica/arquivos/producao/Senna.jpg',NULL,NULL,'Banner (A4)',NULL),('2016-02-23 20:50:57',1,'dvadvadvadv',11,1,NULL,NULL,NULL,'/Library/WebServer/Documents/comunica/arquivos/producao/Angularbg.jpg',NULL,NULL,'Banner (A3)',NULL);
-/*!40000 ALTER TABLE `producao` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id` int(11) NOT NULL,
   `nome` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `senha` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `matricula` varchar(255) COLLATE latin1_general_ci NOT NULL,
@@ -238,28 +172,136 @@ CREATE TABLE `usuario` (
   `status` int(1) NOT NULL,
   `endereco` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
   `informacao` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `usuario`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (20,'Admin Comunica','21232f297a57a5a743894a0e4a801fc3','123456789','admin@comunica.com','(99) 99999-9999',2,1,'Lorem ipsum dolor sit amet, VV',NULL,'2016-02-19 06:01:11'),(44,'Otavio Barbosa','e8d95a51f3af4a3b134bf6bb680a213a','1831408','otavio.barbosa@ifrn.edu.br','8488033222',2,1,'Rua Jornalista Francisco Sinedino',NULL,'2016-02-22 18:48:57');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `emprestimo`
+--
+ALTER TABLE `emprestimo`
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_usuario_emprestimo` (`id_usuario`), ADD KEY `fk_material_emprestimo` (`id_material`), ADD KEY `fk_local_emprestimo` (`id_local`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `eventos`
+--
+ALTER TABLE `eventos`
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_usuario_eventos` (`id_usuario`);
+
+--
+-- Indexes for table `impressao`
+--
+ALTER TABLE `impressao`
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_usuario_impressao` (`id_usuario`);
+
+--
+-- Indexes for table `local`
+--
+ALTER TABLE `local`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `noticias`
+--
+ALTER TABLE `noticias`
+ ADD PRIMARY KEY (`id`), ADD KEY `status` (`status`), ADD KEY `status_2` (`status`), ADD KEY `fk_usuario_noticias` (`id_usuario`);
+
+--
+-- Indexes for table `producao`
+--
+ALTER TABLE `producao`
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_usuario_producao` (`id_usuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emprestimo`
+--
+ALTER TABLE `emprestimo`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `eventos`
+--
+ALTER TABLE `eventos`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `impressao`
+--
+ALTER TABLE `impressao`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `material`
+--
+ALTER TABLE `material`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `noticias`
+--
+ALTER TABLE `noticias`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `producao`
+--
+ALTER TABLE `producao`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `emprestimo`
+--
+ALTER TABLE `emprestimo`
+ADD CONSTRAINT `fk_local_emprestimo` FOREIGN KEY (`id_local`) REFERENCES `local` (`id`),
+ADD CONSTRAINT `fk_material_emprestimo` FOREIGN KEY (`id_material`) REFERENCES `material` (`id`),
+ADD CONSTRAINT `fk_usuario_emprestimo` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `eventos`
+--
+ALTER TABLE `eventos`
+ADD CONSTRAINT `fk_usuario_eventos` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `impressao`
+--
+ALTER TABLE `impressao`
+ADD CONSTRAINT `fk_usuario_impressao` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `noticias`
+--
+ALTER TABLE `noticias`
+ADD CONSTRAINT `fk_usuario_noticias` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `producao`
+--
+ALTER TABLE `producao`
+ADD CONSTRAINT `fk_usuario_producao` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-02-23 17:54:43
